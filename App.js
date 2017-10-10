d204e0f34ce3803de77f64fe740394ce0f2a0525
Seno @ ThinkPad <senoadiw@gmail.com>
2017-10-10 18:09:31 +0700

    tutorial #4

--- App.js
+++ App.js
@@ -1,26 +1,19 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
 
 import store from './store';
+import MainScreen from './screens/MainScreen';
 
 export default class App extends React.Component {
   render() {
+    const MainNavigator = StackNavigator({
+      Main: { screen: MainScreen }
+    });
     return (
       <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+        <MainNavigator />
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
